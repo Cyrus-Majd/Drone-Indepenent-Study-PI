58c723fa21e26508c4d4e13af34ed082a101a8e3
Mohamad Khawam <khawammohamad99@gmail.com>
2023-04-28 14:56:05 -0400

    MutliThreaded cameras

--- server.py
+++ server.py
@@ -21,8 +21,29 @@ app.jinja_env.add_extension("pypugjs.ext.jinja.PyPugJSExtension")
 
 app.register_blueprint(drone_api.drone)
 
+# camera_forward = cv2.VideoCapture(0)
+# camera_down = cv2.VideoCapture(1)
+
+
+# def gen_frames(camera):
+#     while True:
+#         success, frame = camera.read()
+#         if not success:
+#             print("failed", flush=True)
+#             break
+#         else:
+#             ret, buffer = cv2.imencode(".jpg", frame)
+#             frame = buffer.tobytes()
+#             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+import threading
+
 camera_forward = cv2.VideoCapture(0)
 camera_down = cv2.VideoCapture(1)
+
+
+@app.route("/")
+def index():
+    return render_template("index.pug")
 
 
 def gen_frames(camera):
@@ -37,11 +58,6 @@ def gen_frames(camera):
             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 
-@app.route("/")
-def index():
-    return render_template("index.pug")
-
-
 @app.route("/front_feed")
 def front_feed():
     print("Handling front_feed request", flush=True)
@@ -53,10 +69,36 @@ def front_feed():
 
 @app.route("/down_feed")
 def down_feed():
+    print("Handling down_feed request", flush=True)
     print(request.url)
     return Response(
         gen_frames(camera_down), mimetype="multipart/x-mixed-replace; boundary=frame"
     )
+
+
+# start camera threads
+def start_camera_threads():
+    forward_thread = threading.Thread(target=gen_frames, args=(camera_forward,))
+    down_thread = threading.Thread(target=gen_frames, args=(camera_down,))
+    forward_thread.start()
+    down_thread.start()
+
+
+# @app.route("/front_feed")
+# def front_feed():
+#     print("Handling front_feed request", flush=True)
+#     print(request.url)
+#     return Response(
+#         gen_frames(camera_forward), mimetype="multipart/x-mixed-replace; boundary=frame"
+#     )
+
+
+# @app.route("/down_feed")
+# def down_feed():
+#     print(request.url)
+#     return Response(
+#         gen_frames(camera_down), mimetype="multipart/x-mixed-replace; boundary=frame"
+#     )
 
 
 @app.route("/public/<path:path>")
@@ -73,4 +115,5 @@ def current_image(camera="down"):
 
 
 if __name__ == "__main__":
+    start_camera_threads()
     app.run(host=os.getenv("HOST"), port=os.getenv("PORT"), debug=False, threaded=True)
