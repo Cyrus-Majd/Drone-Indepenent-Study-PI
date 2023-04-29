86143db0a40450edb9e4330aaea329138478aed2
Mohamad Khawam <khawammohamad99@gmail.com>
2023-04-29 03:03:22 -0400

    Added Manual opencv

--- public/scripts/opencv.js
+++ public/scripts/opencv.js
@@ -1,21 +1,25 @@
 function onOpenCvReady() {
     // Retrieve the img element
-    var imgElement = document.getElementById('webcam_down');
-    imgElement.onload = function me() {
-        // Create a new canvas element to display the image
-        var canvas = document.createElement('canvas');
-        canvas.width = imgElement.width;
-        canvas.height = imgElement.height;
+    imgElement.addEventListener('change', function me() {
 
-        // Get the canvas context and create an OpenCV image
-        var context = canvas.getContext('2d');
-        var cvImage = cv.imread(imgElement);
-
-        // Display the OpenCV image on the canvas
-        cv.imshow(canvas, cvImage);
 
         // Replace the img element with the canvas element
         imgElement.parentNode.replaceChild(canvas, imgElement);
-    }
+    })
 
+}
+function RunImage() {
+    var imgElement = document.getElementById('webcam_down');
+
+    // Create a new canvas element to display the image
+    var canvas = document.getElementById('outputCanvas');
+    canvas.width = imgElement.width;
+    canvas.height = imgElement.height;
+
+    // Get the canvas context and create an OpenCV image
+    //  var context = canvas.getContext('2d');
+    var cvImage = cv.imread(imgElement);
+
+    // Display the OpenCV image on the canvas
+    cv.imshow(canvas, cvImage);
 }
